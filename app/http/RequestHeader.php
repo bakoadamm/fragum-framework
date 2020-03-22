bccd18c58ae666f54b39a7448c30f329245a4085
szenpapa <benjamin.szalai@melkweg.hu>
2020-03-22 15:30:41 +0100

    http status codes and headers improvements

---
+++ app/http/RequestHeader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lapos
+ * Date: 2020.03.22.
+ * Time: 15:26
+ */
+
+namespace App\http;
+
+
+abstract class RequestHeader
+{
+
+}
--- app/http/StatusCode.php
+++ app/http/StatusCode.php
@@ -12,7 +12,7 @@ namespace App\http;
 abstract class StatusCode
 {
     //1xx informational
-    const CONTINUE = 100;
+    const CONTINUE_ = 100;
 
     const SWITCHING_PROTOCOLS = 101;
 
@@ -89,4 +89,34 @@ abstract class StatusCode
 
     const REQUEST_URI_TOO_LARGE = 414;
 
+    //TODO: add many 4xx status codes - https://www.restapitutorial.com/httpstatuscodes.html
+
+    //5xx server error
+    const INTERNAL_SERVER_ERROR = 500;
+
+    const NOT_IMPLEMENTED = 501;
+
+    const BAD_GATEWAY = 502;
+
+    const SERVICE_UNAVAILABLE = 503;
+
+    const GATEWAY_TIMEOUT = 504;
+
+    const HTTP_VERSION_NOT_SUPPORTED = 505;
+
+    const VARIANT_ALSO_NEGOTIATES = 506;
+
+    const INSUFFICIENT_STORAGE = 507;
+
+    const LOOP_DETECTED = 508;
+
+    const BANDWIDTH_LIMIT_EXCEEDED = 509;
+
+    const NOT_EXTENDED = 510;
+
+    const NETWORK_AUTHENTICATED_REQUIRED = 511;
+
+    const NETWORK_READ_TIMEOUT_ERROR = 598;
+
+    const NETWORK_CONNECT_TIMEOUT_ERROR = 599;
 }
